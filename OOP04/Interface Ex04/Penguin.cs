1265f235f77287849c95ea34421c4ecd6fd66552
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-07 19:22:14 +0200

    Interface Ex04

---
+++ OOP04/Interface Ex04/Penguin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interface_Ex04
+{
+    internal class Penguin : IBird
+    {
+        public void eat()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+        public void Walk()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
