fe29ea972701d7744f1d9036d61df2ef75ea892a
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-08 18:38:51 +0200

    part01-Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -8,8 +8,12 @@
             #region Q1:What is the primary purpose of an interface in C#?
             //b) To define a blueprint for a class
             #endregion
+            #region Q2:Which of the following is NOT a valid access modifier for interface members in C#?
+            //a) private
 
             #endregion
-        }
+
+        #endregion
+    }
     }
 }
