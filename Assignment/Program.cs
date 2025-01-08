042b49d990c1f49a40d26f8389c5a26a2de54326
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-08 18:37:40 +0200

    Part01-Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+            #region Q1:What is the primary purpose of an interface in C#?
+            //b) To define a blueprint for a class
+            #endregion
+
+            #endregion
+        }
+    }
+}
